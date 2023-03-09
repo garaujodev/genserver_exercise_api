11539c36176a2ed92114a0445220239091b8b0ba
Gustavo Araujo <garaujodev@gmail.com>
2023-03-09 01:37:29 -0300

    Add `render` action to `UserController`

--- lib/ranking_web/controllers/user_controller.ex
+++ lib/ranking_web/controllers/user_controller.ex
@@ -1,6 +1,11 @@
 defmodule RankingWeb.UserController do
   use RankingWeb, :controller
 
+  alias Ranking.Worker
+
   def index(conn, _params) do
+    data = Worker.get()
+
+    render(conn, :index, data: data)
   end
 end
